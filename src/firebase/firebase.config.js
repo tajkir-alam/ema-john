b55ae2d7665f3585919a31b89f7297308c8f31fb
tajkir-alam <tajkir.alam@gmail.com>
2023-04-24 22:35:35 +0600

    firebase and firebase hosting installed

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBc8F9UkzId-ic7nBc2TCSep57fk92uVeU",
+  authDomain: "ema-john-auth-form-1st.firebaseapp.com",
+  projectId: "ema-john-auth-form-1st",
+  storageBucket: "ema-john-auth-form-1st.appspot.com",
+  messagingSenderId: "939089277685",
+  appId: "1:939089277685:web:e616de92035f4831298bbc"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
